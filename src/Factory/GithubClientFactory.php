0e46748bef617ab227266ad3196558802cdee318
Rokas Mikalkėnas <mikalkenas@gmail.com>
2018-01-28 17:49:11 +0200

    Cache middleware added.

--- src/Factory/GithubClientFactory.php
+++ src/Factory/GithubClientFactory.php
@@ -4,9 +4,13 @@ namespace App\Factory;
 
 use App\Client\GithubClient;
 use App\Manager\GithubUserManager;
+use Doctrine\Common\Cache\FilesystemCache;
 use GuzzleHttp\Client;
 use GuzzleHttp\Command\Guzzle\Description;
 use GuzzleHttp\Middleware;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage;
+use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
 use Psr\Http\Message\RequestInterface;
 use GuzzleHttp\HandlerStack;
 
@@ -15,12 +19,17 @@ class GithubClientFactory
     /**
      * @param Description $description
      * @param GithubUserManager $userManager
+     * @param string $cacheDir
      * @return GithubClient
      */
-    public function create(Description $description, GithubUserManager $userManager): GithubClient
+    public function create(Description $description, GithubUserManager $userManager, string $cacheDir): GithubClient
     {
+        $handlerStack = HandlerStack::create();
+        $handlerStack->push($this->getMapRequestMiddleware($userManager->getToken()), 'map_request');
+        $handlerStack->push($this->getCacheMiddleware($cacheDir), 'cache');
+
         return new GithubClient(
-            new Client(['handler' => $this->getMiddlewaresStack($userManager->getToken())]),
+            new Client(['handler' => $handlerStack]),
             $description,
             null,
             null,
@@ -31,15 +40,27 @@ class GithubClientFactory
 
     /**
      * @param string $authToken
-     * @return HandlerStack
+     * @return callable
      */
-    protected function getMiddlewaresStack(string $authToken): HandlerStack
+    protected function getMapRequestMiddleware(string $authToken): callable
     {
-        $handlerStack = HandlerStack::create();
-        $handlerStack->push(Middleware::mapRequest(function (RequestInterface $request) use ($authToken) {
+        return Middleware::mapRequest(function (RequestInterface $request) use ($authToken) {
             return $request->withHeader('Authorization', sprintf('token %s', $authToken));
-        }));
+        });
+    }
 
-        return $handlerStack;
+    /**
+     * @param string $cacheDir
+     * @return callable
+     */
+    protected function getCacheMiddleware(string $cacheDir): callable
+    {
+        return new CacheMiddleware(
+            new PrivateCacheStrategy(
+                new DoctrineCacheStorage(
+                    new FilesystemCache($cacheDir . '/github_client')
+                )
+            )
+        );
     }
 }
